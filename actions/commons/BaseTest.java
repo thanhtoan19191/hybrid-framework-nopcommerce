4b76e0a19800cd0bda50ea433191c3949da15966
thanhtoan19191 <thanhtoan19191@hotmail.com>
2021-11-28 17:13:37 +0700

    Exercise multiple browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,29 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseTest {
 
+	private String projectPath = System.getProperty("user.dir");
+	private WebDriver driver;
+	
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}else {
+			throw new RuntimeException("Browser name invalid");
+		}
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+		
+		return driver;
+	}
 }
