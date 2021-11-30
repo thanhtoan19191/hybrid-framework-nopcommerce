864344bff174c79ddefb3138ffbd96c1921dda2c
thanhtoan19191 <thanhtoan19191@hotmail.com>
2021-11-30 11:33:35 +0700

    Exercise page factory

---
+++ actions/commons/BasePageFactory.java
@@ -0,0 +1,140 @@
+package commons;
+
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePageFactory {
+
+	public void openPageURL(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+
+	public String getPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+
+	public Alert waitForAlertPresence(WebDriver driver) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		return explicitWait.until(ExpectedConditions.alertIsPresent());
+	}
+
+	public void acceptAlert(WebDriver driver) {
+		waitForAlertPresence(driver).accept();
+	}
+
+	public void cancelAlert(WebDriver driver) {
+		waitForAlertPresence(driver).dismiss();
+	}
+
+	public String getAlertText(WebDriver driver) {
+		return waitForAlertPresence(driver).getText();
+	}
+
+	public void sendkeyToAlert(WebDriver driver, String textValue) {
+		waitForAlertPresence(driver).sendKeys(textValue);
+	}
+
+	public void switchToWindowByID(WebDriver driver, String windowID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String id : allWindowIDs) {
+			if (!id.equals(windowID)) {
+				driver.switchTo().window(id);
+				break;
+			}
+		}
+	}
+
+	public void switchToWindowByPageTitle(WebDriver driver, String expectedPageTitle) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String window : allWindows) {
+			driver.switchTo().window(window);
+			String actualPageTitle = driver.getTitle();
+			if (actualPageTitle.equals(expectedPageTitle)) {
+				break;
+			}
+		}
+	}
+
+	public void closeAllWindowWithoutParent(WebDriver driver, String parentPageID) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String window : allWindows) {
+			if (!window.equals(parentPageID)) {
+				driver.switchTo().window(window);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentPageID);
+	}
+	
+	public void waitForElementVisible(WebDriver driver, WebElement element) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		explicitWait.until(ExpectedConditions.visibilityOf(element));
+	}
+	
+	public void waitForAllElementsVisible(WebDriver driver, List<WebElement> elements) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		explicitWait.until(ExpectedConditions.visibilityOfAllElements(elements));
+	}
+	
+	public void waitForElementInVisible(WebDriver driver, WebElement element) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		explicitWait.until(ExpectedConditions.invisibilityOf(element));
+	}
+	
+	public void waitForAllElementsInVisible(WebDriver driver, List<WebElement> elements) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		explicitWait.until(ExpectedConditions.invisibilityOfAllElements(elements));
+	}
+	
+	public void waitForElementClickable(WebDriver driver, WebElement element) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, longTimeout);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+	
+	public void clickToElement(WebDriver driver, WebElement element) {
+		element.click();
+	}
+	
+	public void sendkeyToElement(WebDriver driver, WebElement element, String textValue) {
+		element.clear();
+		element.sendKeys(textValue);
+	}
+	
+	public String getElementText(WebDriver driver, WebElement element) {
+		return  element.getText();
+	}
+	
+	public boolean isElementDisplayed(WebDriver driver, WebElement element) {
+		return element.isDisplayed();
+	}
+	
+	
+	private long longTimeout = 30;
+	private long shortTimeout = 5;
+}
---
+++ testcases/com/nopcommerce/user/Level_05_Page_Factory.java
@@ -0,0 +1,148 @@
+package com.nopcommerce.user;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+import commons.BaseTest;
+import pageObjects.HomePageObject;
+import pageObjects.LoginPageObject;
+import pageObjects.RegisterPageObject;
+
+public class Level_05_Page_Factory extends BaseTest {
+	
+	
+	private WebDriver driver;
+	private String firstName, lastName, invalidEmail, notFoundEmail,existingEmail,validPassword, incorrectPassword;
+	private HomePageObject homePage ;
+	private RegisterPageObject registerPage ;
+	private LoginPageObject loginPage;
+	
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		
+		
+		driver = getBrowserDriver(browserName);
+		
+		
+		firstName="Automation";
+		lastName="FC";
+		validPassword = "123456";
+		incorrectPassword="654321";
+		invalidEmail="123@345&*";
+		existingEmail = "auto" + generateFakeNumber() + "@mail.com";
+		notFoundEmail="auto" + generateFakeNumber() + "@mail.vn";
+		
+		homePage = new HomePageObject(driver);
+		System.out.println("Precondition - Step 01: Click to Register link");
+		homePage.clickToRegisterLink();
+		registerPage = new RegisterPageObject(driver);
+		
+		
+		System.out.println("Precondition - Step 02: Input to required fields");
+		registerPage.inputToFirstnameTextbox(firstName);
+		registerPage.inputToLastnameTextbox(lastName);
+		registerPage.inputToEmailTextbox(existingEmail);
+		registerPage.inputToPasswordTextbox(validPassword);
+		registerPage.inputToConfirmPasswordTextbox(validPassword);
+
+		System.out.println("Precondition - Step 03: Click to Register button");
+		registerPage.clickToRegisterButton();
+		
+		System.out.println("Precondition - Step 04: Verify success message displayed");
+		Assert.assertEquals(registerPage.getRegisterSuccessMessage(),"Your registration completed");
+		
+		System.out.println("Precondition - Step 05: Click to Logout link");
+		registerPage.clickToLogoutLink();
+		
+		homePage = new HomePageObject(driver);
+	}
+
+	@Test
+	public void Login_01_Empty_Data() {
+		homePage.clickToLoginLink();
+		
+		loginPage = new LoginPageObject(driver);
+
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(),"Please enter your email");
+		
+		
+		
+
+	}
+
+	@Test
+	public void Login_02_Invalid_Email() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputToEmailTextbox(invalidEmail);
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextbox(),"Wrong email");
+
+
+	}
+
+	@Test
+	public void Login_03_Email_Not_Found() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputToEmailTextbox(notFoundEmail);
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageUnsuccessfull(),"Login was unsuccessful. Please correct the errors and try again.\nNo customer account found");
+	}
+
+	@Test
+	public void Login_04_Existing_Email_Empty_Password() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputToEmailTextbox(existingEmail);
+		loginPage.inputToPasswordTextbox("");
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageUnsuccessfull(),"Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect");
+
+	}
+
+	@Test
+	public void Login_05_Existing_Email_Incorrect_Password() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputToEmailTextbox(existingEmail);
+		loginPage.inputToPasswordTextbox(incorrectPassword);
+		loginPage.clickToLoginButton();
+		Assert.assertEquals(loginPage.getErrorMessageUnsuccessfull(),"Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect");
+	}
+
+	@Test
+	public void Login_06_Valid_Email_Password() {
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputToEmailTextbox(existingEmail);
+		loginPage.inputToPasswordTextbox(validPassword);
+		loginPage.clickToLoginButton();
+		Assert.assertTrue(homePage.isMyAccountLinkDisplayed());
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int generateFakeNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+
+}
