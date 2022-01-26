c082f4ae267f365d40f804337c06dee722b7fa4d
thanhtoan19191 <thanhtoan19191@hotmail.com>
2022-01-26 19:08:55 +0700

    exercise sort

--- actions/pageObjects/nopCommerce/user/UserHomePageObject.java
+++ actions/pageObjects/nopCommerce/user/UserHomePageObject.java
@@ -1,11 +1,17 @@
 package pageObjects.nopCommerce.user;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import commons.BasePage;
 import commons.PageGeneratorManager;
 import io.qameta.allure.Step;
 import pageUIs.nopCommerce.user.HomePageUI;
+import pageUIs.saucelab.InventoryPageUI;
 
 public class UserHomePageObject extends BasePage {
 	private WebDriver driver;
@@ -49,6 +55,68 @@ public class UserHomePageObject extends BasePage {
 		waitForElementVisible(driver, HomePageUI.SEARCH_FOOTER_LINK);
 		clickToElement(driver, HomePageUI.SEARCH_FOOTER_LINK);
 		return PageGeneratorManager.getUserSearchPage(driver);
+	}
+
+	public void clickToSubMenu() {
+		waitForElementClickable(driver, HomePageUI.COMPUTERS_LINK);
+		clickToElement(driver, HomePageUI.COMPUTERS_LINK);
+		waitForElementClickable(driver, HomePageUI.NOTEBOOKS_LINK);
+		clickToElement(driver, HomePageUI.NOTEBOOKS_LINK);
+		
+	}
+
+	public void selectItemInSortDropdown(String itemText) {
+		waitForElementClickable(driver, HomePageUI.SORT_DROPDOWN);
+		selectItemInDefaultDropdown(driver, HomePageUI.SORT_DROPDOWN, itemText);
+		
+	}
+
+	public boolean isProductNameSortAcseDing() {
+		List<WebElement> productNameElements = getListWebElement(driver, HomePageUI.PRODUCT_NAME_TEXT);
+		
+		List<String> productNameText = new ArrayList<String>();
+		
+		for(WebElement productName:productNameElements) {
+			productNameText.add(productName.getText());
+		}
+		
+		List<String> productNameTextClone = new ArrayList<String>(productNameText);
+		
+		Collections.sort(productNameTextClone);
+		return productNameText.equals(productNameTextClone);
+	}
+
+	public boolean isProductNameSortDescenDing() {
+		List<WebElement> productNameElements = getListWebElement(driver, HomePageUI.PRODUCT_NAME_TEXT);
+		
+		List<String> productNameText = new ArrayList<String>();
+		
+		for(WebElement productName:productNameElements) {
+			productNameText.add(productName.getText());
+		}
+		
+		List<String> productNameTextClone = new ArrayList<String>(productNameText);
+		
+		Collections.sort(productNameTextClone);
+		Collections.reverse(productNameTextClone);
+		
+		return productNameText.equals(productNameTextClone);
+	}
+
+	public boolean isProductPriceSortAcseDing() {
+		List<WebElement> productPriceElements = getListWebElement(driver, HomePageUI.PRODUCT_PRICE_TEXT);
+		
+		List<Float> productPriceValue = new ArrayList<Float>();
+		
+		for(WebElement productPrice:productPriceElements) {
+			Float productPriceNumber = Float.parseFloat(productPrice.getText().replace("$", "")) ;
+			productPriceValue.add(productPriceNumber);
+		}
+		
+		List<Float> productPriceTextClone = new ArrayList<Float>(productPriceValue);
+		
+		Collections.sort(productPriceTextClone);
+		return productPriceValue.equals(productPriceTextClone);
 	}
 
 	
